666bf9d26567edc7911ea8af236c6a20e9d5d9c5
Siddhartha Kanugu <luckysiddartha25@gmail.com>
2025-12-28 07:28:04 -0600

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
@@ -14,21 +14,22 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); // req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 //make ready for deployment
-if(ENV.NODE_ENV === "production") {
+if (ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("*", (_,res) => {
+    app.get("*", (_, res) => {
         res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
 
 app.listen(PORT, () => {
-     console.log("Server running on port "+ PORT);
-     connectDB();
+    console.log("Server running on port " + PORT);
+    connectDB();
 });
