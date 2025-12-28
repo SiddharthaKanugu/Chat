9fa6a5cf72d6353d06deacb5f0e0ee7067e1b5fa
Siddhartha Kanugu <luckysiddartha25@gmail.com>
2025-12-28 03:56:13 -0600

    login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,27 +5,27 @@ import bcrypt from "bcryptjs";
 import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body
+    const { fullName, email, password } = req.body
 
     try {
-        if(!fullName || !email || !password){
-            return res.status(400).json({message:"All fields are required"})
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
         }
 
-        if(password.length <6) {
-            return res.status(400).json({message:"password must be atleast 6 characters"})
+        if (password.length < 6) {
+            return res.status(400).json({ message: "password must be atleast 6 characters" })
         }
 
         //check if email is valid or not
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if(!emailRegex.test(email)) {
-            return res.status(400).json({message:"Invalid email format"});
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
         }
-        const user = await User.findOne({email});
-        if(user) return res.status(400).json({message:"Email already exists"}) 
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "Email already exists" })
 
         const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
+        const hashedPassword = await bcrypt.hash(password, salt)
 
         const newUser = new User({
             fullName,
@@ -33,18 +33,18 @@ export const signup = async (req, res) => {
             password: hashedPassword
         })
 
-        if(newUser) {
+        if (newUser) {
             //generateToken(newUser.id,res)
             //await newUser.save()
 
-        const savedUser = await newUser.save();
-        generateToken(savedUser._id, res);
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
 
             res.status(201).json({
-                _id:newUser._id,
-                fullName:newUser.fullname,
-                email:newUser.email,
-                profilePic:newUser.profilePic,
+                _id: newUser._id,
+                fullName: newUser.fullname,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
             })
 
             try {
@@ -54,11 +54,42 @@ export const signup = async (req, res) => {
             }
 
         } else {
-            res.status(400).json({message: "Invalid user data"})
+            res.status(400).json({ message: "Invalid user data" })
         }
-        
+
     } catch (error) {
-        console.log("Error in signup controller:",error)
-        res.status(500).json({message:"Internal server error"});
+        console.log("Error in signup controller:", error)
+        res.status(500).json({ message: "Internal server error" });
     }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.findOne({ email })
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+
+    } catch (error) {
+        console.error("Error in login controller:", error)
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out succesfully" });
 };
